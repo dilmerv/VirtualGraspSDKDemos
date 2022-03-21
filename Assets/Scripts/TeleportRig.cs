9bcdfa25529209d50634bf4b9a20540a6256077d
Dilmer Valecillos <dilmervst@gmail.com>
2022-03-20 21:54:46 -0600

    Fixed teleportation

--- Assets/Scripts/TeleportRig.cs
+++ Assets/Scripts/TeleportRig.cs
@@ -3,23 +3,16 @@ using UnityEngine;
 public class TeleportRig : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] targets;
+    private GameObject target;
 
     [SerializeField]
     private GameObject[] rigComponents;
 
-    private int currentTarget;
-
     public void Teleport()
     {
-        if (targets?.Length == 0|| rigComponents?.Length == 0)
-            Debug.LogError("Teleport Rig is not configured");
-
-        foreach (var c in rigComponents)
+        foreach (var r in rigComponents)
         {
-            c.transform.position = new Vector3(targets[0].transform.position.x, c.transform.position.y, c.transform.position.z);
-            currentTarget++;
-            if (currentTarget == targets.Length) currentTarget = 0;
+            r.transform.position = new Vector3(target.transform.position.x, r.transform.position.y, r.transform.position.z);
         }
     }
 }
